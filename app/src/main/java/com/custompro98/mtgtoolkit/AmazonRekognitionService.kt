b73ad04205fc4b06ff4bce4067c6cd2282183de8
custompro98 <mitchjoa@gmail.com>
2019-01-26 18:46:16 -0600

    Refactor Amazon Rekognition to its own service

---
+++ app/src/main/java/com/custompro98/mtgtoolkit/AmazonRekognitionService.kt
@@ -0,0 +1,44 @@
+package com.custompro98.mtgtoolkit
+
+import android.content.Context
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.rekognition.AmazonRekognitionClient
+import com.amazonaws.services.rekognition.model.DetectTextRequest
+import com.amazonaws.services.rekognition.model.Image
+import java.io.File
+import java.nio.ByteBuffer
+
+class AmazonRekognitionService(image: File, private var context: Context) {
+    private val byteBuffer: ByteBuffer
+    private val awsCredentialsProvider: AWSCredentialsProvider
+    private val amazonRekognitionClient: AmazonRekognitionClient
+
+    init {
+        this.byteBuffer = ByteBuffer.wrap(image.readBytes())
+        this.awsCredentialsProvider = getAwsCredentialsProvider()
+        this.amazonRekognitionClient = AmazonRekognitionClient(awsCredentialsProvider)
+    }
+
+    fun parse(): String {
+        val detectTextRequest = DetectTextRequest()
+                .withImage(Image()
+                        .withBytes(byteBuffer))
+
+        val detectTextResult = amazonRekognitionClient.detectText(detectTextRequest)
+        val probableCardName = detectTextResult.textDetections.find { textDetection ->
+            textDetection.detectedText.length > 1
+        }
+
+        return probableCardName?.detectedText ?: "No card title found"
+    }
+
+    private fun getAwsCredentialsProvider(): AWSCredentialsProvider {
+        return CognitoCachingCredentialsProvider(
+                context,
+                "us-east-2:1c8019f2-99e6-481e-934c-94fcaaacaf72",
+                Regions.US_EAST_2
+        )
+    }
+}
--- app/src/main/java/com/custompro98/mtgtoolkit/ParseImageTask.kt
+++ app/src/main/java/com/custompro98/mtgtoolkit/ParseImageTask.kt
@@ -3,17 +3,19 @@ package com.custompro98.mtgtoolkit
 import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.view.View
-import com.amazonaws.auth.CognitoCachingCredentialsProvider
-import com.amazonaws.regions.Regions
-import com.amazonaws.services.rekognition.AmazonRekognitionClient
-import com.amazonaws.services.rekognition.model.DetectTextRequest
-import com.amazonaws.services.rekognition.model.Image
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
-import java.nio.ByteBuffer
 
 @SuppressLint("StaticFieldLeak")
-class ParseImageTask(private var activity: MainActivity, private var imagePath: String) : AsyncTask<Void, Void, String>() {
+class ParseImageTask(private var activity: MainActivity, imagePath: String) : AsyncTask<Void, Void, String>() {
+    private val image: File
+    private val parsingService: AmazonRekognitionService
+
+    init {
+        this.image = File(imagePath)
+        this.parsingService = AmazonRekognitionService(image, activity.applicationContext)
+    }
+
     override fun onPreExecute() {
         super.onPreExecute()
 
@@ -22,38 +24,14 @@ class ParseImageTask(private var activity: MainActivity, private var imagePath: 
     }
 
     override fun doInBackground(vararg p0: Void?): String {
-        val file = File(imagePath)
-        val byteBuffer = ByteBuffer.wrap(file.readBytes())
-
-        val awsCredentials = CognitoCachingCredentialsProvider(
-                activity.applicationContext,
-                "us-east-2:1c8019f2-99e6-481e-934c-94fcaaacaf72",
-                Regions.US_EAST_2
-        )
-
-        val amazonRekognitionClient = AmazonRekognitionClient(awsCredentials)
-        val detectTextRequest = DetectTextRequest()
-                .withImage(Image()
-                        .withBytes(byteBuffer))
-
-        var returnValue: String
-
-        try {
-            val result = amazonRekognitionClient.detectText(detectTextRequest)
-            val probableCardName = result.textDetections.find { textDetection ->
-                textDetection.detectedText.length > 1
-            }
-
-            returnValue = probableCardName?.detectedText ?: "No card title found"
+        return try {
+            parsingService.parse()
         } catch (e: Exception) {
             e.printStackTrace()
-
-            returnValue = "Error!"
+            "Error!"
         } finally {
-            file.delete()
+            image.delete()
         }
-
-        return returnValue
     }
 
     override fun onPostExecute(result: String?) {
